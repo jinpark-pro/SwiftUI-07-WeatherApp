dee204f3860da0e633426c493a3b1794d5576a49
Jin Park <jinpark1504@gmail.com>
2024-06-25 11:11:58 +0900

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Jungjin Park on 2024-06-25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
